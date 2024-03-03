d027134ca4a9dfbcb4f5ee67af52761a741902f4
Maria Alemaskina <maria131621@yandex.ru>
2024-03-03 18:05:44 +0300

    feat: add users entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { OffersModule } from './offers/offers.module';
+import { WishesModule } from './wishes/wishes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    OffersModule,
+    WishesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,52 @@
+import { IsDate, IsEmail, IsString, IsUrl, Length } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  @IsDate()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @IsDate()
+  updatedAt: Date;
+
+  @Column({
+    unique: true,
+  })
+  @IsString()
+  @Length(2, 30)
+  username: string;
+
+  @Column({
+    default: 'Пока ничего не рассказал о себе',
+  })
+  @Length(2, 200)
+  @IsString()
+  about: string;
+
+  @Column({
+    default: 'https://i.pravatar.cc/300',
+  })
+  @IsUrl()
+  avatar: string;
+
+  @Column({
+    unique: true,
+  })
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @IsString()
+  password: string;
+}
